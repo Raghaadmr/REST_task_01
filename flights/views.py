19fdffd81a3bee94cebeab293ee2724a1869ac5a
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-09-15 00:37:42 +0300

    completed task1

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+import datetime
+from django.utils import timezone
+from rest_framework.generics import ListAPIView
+from .serializers import FlightSerializer, BookingSerializer
+
+class FlightsList(ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightSerializer
+
+
+class BookingsList(ListAPIView):
+	queryset = Booking.objects.filter(date__gt=datetime.date.today())
+	serializer_class = BookingSerializer
--- task_1/settings.py
+++ task_1/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'flights',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
